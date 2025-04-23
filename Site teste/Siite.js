09ec766f67d5f3c8f0760326ef48defdad16e34a
Ronaldgoes <ronaldbentogoes@gmail.com>
2025-04-22 21:53:24 -0300

    Atualização aula HTML e JS

---
+++ Site teste/Siite.js
@@ -0,0 +1,30 @@
+function EX1(){
+    let num1 = Number (prompt("Digite um Número"))
+    let num2 = Number (prompt("Digite outro Número"))
+    let soma = num1 + num2
+    alert("A soma dos dois números é: " + soma)
+}
+
+
+
+function EX2(){
+    let num1 = Number (prompt("Digite um Número"))
+    let num2 = Number (prompt("Digite outro Número"))
+    let media = (num1 + num2) / 2
+    alert("A media dos dois números é: " + media)
+}
+
+function NOME(){
+
+let name = prompt("Digite seu Nome: ")
+alert("Olá," + name + "! Seja bemvindo!")
+
+}
+
+function TEMPERATURA(){
+
+    let temperatura = Number (prompt("Digite a Temperatura em Celsius: "))
+    let Fahrenheit = temperatura * 9/5 + 32
+    alert("A temperatura em Fahrenheit é de: " + Fahrenheit)
+    
+    }
