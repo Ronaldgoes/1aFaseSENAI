25acbafa6092ec283203496ef0ad5d8e8ed6ef41
Ronaldgoes <ronaldbentogoes@gmail.com>
2025-04-23 17:32:50 -0300

    Atualização das Atividades HTML e JS

--- Site teste/Siite.js
+++ Site teste/Siite.js
@@ -17,7 +17,7 @@ function EX2(){
 function NOME(){
 
 let name = prompt("Digite seu Nome: ")
-alert("Olá," + name + "! Seja bemvindo!")
+alert("Olá," + name + "! Seja bem vindo! ")
 
 }
 
@@ -27,4 +27,55 @@ function TEMPERATURA(){
     let Fahrenheit = temperatura * 9/5 + 32
     alert("A temperatura em Fahrenheit é de: " + Fahrenheit)
     
+}
+
+function PAREIMPAR(){
+
+    let numero = parseInt(prompt("Digite um número:"));
+
+    if (numero % 2 === 0) {
+      alert(`${numero} é par`);
+    } else {
+      alert(`${numero} é ímpar`);
+    }   
+ }
+
+ function MEDIA(){
+    let nota1 = Number (prompt("Digite a primeira nota"))
+    let nota2= Number (prompt("Digite a segunda nota"))
+    let nota3 = Number (prompt("Digite a terceira nota"))
+    let media = (nota1 + nota2 + nota3) / 3
+    alert("A media do aluno é de: " + media)
+}
+    {
+    function IMC(){
+        let peso = Number(prompt("Digite seu peso em kg:"));
+        let altura = Number(prompt("Digite sua altura em metros:"));
+    
+        let imc = peso / (altura * altura);
+    
+        if (imc < 18.5) {
+            alert("Você está abaixo do peso. IMC: " + imc);
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            alert("Você está com peso normal. IMC: " + imc);
+        } else if (imc >= 25 && imc <= 29.9) {
+            alert("Você está com sobrepeso. IMC: " + imc);
+        } else {
+            alert("Você está com obesidade. IMC: " + imc);
+        }
     }
+
+    function BISSEXTO(){
+        let ano = Number(prompt("Digite um ano:"));
+
+    if ((ano % 4 === 0 && ano % 100 !== 0)) {
+        alert(ano + " é um ano bissexto.");
+    } else {
+        alert(ano + " não é um ano bissexto.");
+    }
+    }
+    
+    
+    
+}
+
